d066349d9bef6613f7eaae0db842e66d35616f48
james C. <cj.concepcion7@gmail.com>
2025-03-06 12:45:38 +0800

    sd

--- .history/db/supabase_20250306123200.js
+++
@@ -1,7 +0,0 @@
-// db/supabase.js
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
-
-const SUPABASE_URL = "https://your-supabase-url.supabase.co";
-const SUPABASE_ANON_KEY = "your-anon-key";
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
--- .history/db/supabase_20250306124320.js
+++
@@ -1,7 +0,0 @@
-// db/supabase.js
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
-
-const SUPABASE_URL = "https://ucskgzyiufvooylkdqfi.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVjc2tnenlpdWZ2b295bGtkcWZpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDEyMzQ4NzYsImV4cCI6MjA1NjgxMDg3Nn0.6U5ov63_qHl3XxTgbCNjqeDxppaSSMcC-xHZQjZ86Ik";
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
